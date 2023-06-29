e8c3745995dbc6d114271cbfb6703b1e831680b6
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2023-06-28 20:41:09 -0500

    Guardamos

---
+++ app/src/main/java/com/example/turisteando_v2/HotelesHome.java
@@ -0,0 +1,17 @@
+package com.example.turisteando_v2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HotelesHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hoteles_home);
+
+
+    }
+}
