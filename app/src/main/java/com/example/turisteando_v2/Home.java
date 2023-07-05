b6ab0530713b6fc13acfdbe7ec2796379b5ab8ad
207 <207@academicos.cesde.edu.co>
2023-07-04 20:39:30 -0500

    Se crea menu de opciones

--- app/src/main/java/com/example/turisteando_v2/Home.java
+++ app/src/main/java/com/example/turisteando_v2/Home.java
@@ -3,6 +3,8 @@ package com.example.turisteando_v2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -30,5 +32,38 @@ public class Home extends AppCompatActivity {
                 Toast.makeText(Home.this, "Hice Clic", Toast.LENGTH_SHORT).show();
             }
         });
+        BotonRestaurantes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Home.this, "Hice Clic 2", Toast.LENGTH_SHORT).show();
+            }
+        });
+        BotonTurismo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Home.this, "Hice Clic 3", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    public boolean onCreateOptionMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+    public boolean onOptionItemSelected(MenuItem item){
+        int itemSeleccionado= item.getItemId();
+        switch (itemSeleccionado){
+
+            case(R.id.opcion1):
+                break;
+            case(R.id.opcion2):
+                break;
+            case(R.id.opcion3):
+                break;
+            case(R.id.opcion4):
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
